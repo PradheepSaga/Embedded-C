169194a82590a762c88fb93082ad371baeef2890
PradheepSaga <148743074+PradheepSaga@users.noreply.github.com>
2025-04-04 13:52:29 +0530

    Create while_one.c

---
+++ while_one.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(){
+    while(1); // Executes forever, becoz '1=true' (*The code stucks here)
+    printf("Hello Everyone, Myself Eren Yeager!"); // This statement won't be executed !
+    return 0;
+}
