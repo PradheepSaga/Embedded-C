805bd5ad1c8db677f94505a278e1c013c939940f
PradheepSaga <148743074+PradheepSaga@users.noreply.github.com>
2025-04-04 13:54:35 +0530

    Create while_ex.c

---
+++ while_ex.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    while(1){  
+        printf("I'm Embedded Engineer$\n"); // Runs Infinitely
+    }
+
+    while(0){
+        printf("Myself PradheepSaga"); // Never Runs
+    }
+    return 0;
+}
